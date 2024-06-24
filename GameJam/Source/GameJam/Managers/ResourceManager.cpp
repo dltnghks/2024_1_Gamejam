59793ec31d2f20329598bd4ee1300f7c824d4fb0
dltnghks <dltrnghks@naver.com>
2024-06-24 18:04:44 +0900

    Fix : ResourcManager CPP 수정

--- GameJam/Source/GameJam/Managers/ResourceManager.cpp
+++ GameJam/Source/GameJam/Managers/ResourceManager.cpp
@@ -4,23 +4,11 @@
 #include "ResourceManager.h"
 
 #include "GameJam/GameJamGameMode.h"
+#include "GameJam/Object/ObjSoapBubble.h"
 #include "GameJam/Object/ObjectPool/PoolManager.h"
 
 
-template <typename T>
-T* UResourceManager::Instantiate(TSubclassOf<T> original)
-{
-	if(original.GetDefaultObject()->GetComponentByClass(UPoolable::StaticClass()))
-	{
-		// 오브젝트 풀에서 뽑아오기
-		return _gameMode->PoolManager->Pop(original);
-	}
-
-	T* actor = NewObject<AActor>(this, original.Get());
-	return actor;
-}
-
-void UResourceManager::Destory(AActor* actor)
+void UResourceManager::ObjectDestory(AActor* actor)
 {
 	if(actor == nullptr) return;
 

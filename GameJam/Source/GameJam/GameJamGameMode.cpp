2b25fa82371542a2199c71599c78b1cfc071d277
dltnghks <dltrnghks@naver.com>
2024-06-24 20:15:40 +0900

    Fix : 블루프린트 SoapBubble, GameMode

--- GameJam/Source/GameJam/GameJamGameMode.cpp
+++ GameJam/Source/GameJam/GameJamGameMode.cpp
@@ -26,6 +26,5 @@ void AGameJamGameMode::BeginPlay()
 	UE_LOG(LogTemp, Log, TEXT("GameMode BeginPlay"));
 	AObjSoapBubble* bubble = ResourceManager->Instantiate<AObjSoapBubble>(testClass);
 	bubble->SetActorLocation(FVector(2400.000000f,1360.000000f,315.062601f));
-	
 	UE_LOG(LogTemp, Log, TEXT("%s 생성 완료"), *bubble->GetName());
 }
